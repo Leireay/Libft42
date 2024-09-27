e6117694dd98dafff60fcdc88f32008b0cbbe524
Leireay <163833943+Leireay@users.noreply.github.com>
2024-09-27 22:22:47 +0200

    Create ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,17 @@
+#include <libft.h>
+
+char    *ft_strchr(const char   *s, int c)
+{
+        while (*s) //mientras el puntero en la cadena exista
+        {
+                if (*s == a) //mientras el puntero busca caracter en la cadena que sea igual
+                {
+                        return ((char *) s);
+
+                }
+                s++;
+        }
+        if (a == '\0')
+          return ((char *) s);
+        return (0);
+}
