b511ce3efefed4e9e7ce5dd748438be685da87ec
Leireay <163833943+Leireay@users.noreply.github.com>
2024-09-27 23:56:50 +0200

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,13 +1,12 @@
 #include <libft.h>
 
-char    *ft_strchr(const char   *s, int c)
+char        *ft_strchr(const char *s, int c)
 {
         while (*s) //mientras el puntero en la cadena exista
         {
                 if (*s == c) //mientras el puntero busca caracter en la cadena que sea igual
                 {
                         return ((char *) s);
-
                 }
                 s++;
         }
