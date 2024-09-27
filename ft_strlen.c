269eb474d99372890bef5229def54757beea0fb3
Leireay <163833943+Leireay@users.noreply.github.com>
2024-09-27 21:16:45 +0200

    Create ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,21 @@
+#include <libft.h>
+
+int	ft_strlen(char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	printf("%lu\n", strlen("hola"));
+}*/
