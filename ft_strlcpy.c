a53dde4c2b1d5accfd2e6f521e93dec247f7819a
Leireay <163833943+Leireay@users.noreply.github.com>
2024-09-28 12:24:00 +0200

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,9 +3,13 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
+	size_t	len; //crea una variable para guardar la longitud del string
 
+	len = ft_strlen(src) //guardo la variable len y la longitud de mi string original
+	if (dstsize < 1) //si el tamaño del buffer es menor que 1
+		return (len);//devuelvo la longitud de origen
 	i = 0;
-	if (size > 0)
+	while ()
 	{
 		while (src[i] && i < (size - 1))
 		{
