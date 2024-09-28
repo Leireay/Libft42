38a09ab5d21da603a07bbbb873ad277b123c29c4
Leireay <163833943+Leireay@users.noreply.github.com>
2024-09-28 12:34:59 +0200

    Update ft_strlcpy.c
    Arreglo de norma

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	size_t	len; //crea una variable para guardar la longitud del string
 
 	len = ft_strlen(src) //guardo la variable len y la longitud de mi string original
-	if (dstsize < 1) //si el tamaño del buffer es menor que 1
+		if (dstsize < 1) //si el tamaño del buffer es menor que 1
 		return (len);//devuelvo la longitud de origen
 	i = 0;
 	while (src[i] != '\0' && i < (dstsize - 1))//mientras mi origen en la posicion i sea distinto a caracter nulo.Y mi contador sea menor que mi buffer menos 1 PARA CHAR NULO.
@@ -14,6 +14,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 		dst[i] = src[i];//copio mi origen en el destino
 		i++;//Voy avanzando para recorer el src
 	}
-		dst[i] = '\0';//por ultimo cierro con caracter nulo mi destino
+	dst[i] = '\0';//por ultimo cierro con caracter nulo mi destino
 	return (len);//devuelvo la longitud total de la cadena a crear (longitud de src)
 }
