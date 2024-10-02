1a9247d4e4b2f378eeddbc1f539d8d8a3de125b4
Leireay <163833943+Leireay@users.noreply.github.com>
2024-10-02 13:13:44 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -5,13 +5,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t		i;
 
 	i = 0;  				// inicializo i en cero
-	while (i < n && (s1[i] || s2[i]))	//mientras i sea menor que el tamño de mi buffer y por s1 de i o sa2 de i exista
+	while (i < n && (s1[i] || s2[i]))	//mientras i sea menor que el tamño de mi buffer y por s1 de i o s2 de i exista
 	{
 		if (s1[i] != s2[i])		//si s1 de i es diferente a s2 de i
 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);	// devuelvo la diferencia de la ultima pocision coincidente
 		else if (s1[i] == '\0')		// si s1 en la pocicion y es nulo
-			return (0);		//devuelvo nulo
+			return (0);		//devuelvo null
 		i++;				// aumento el iterdor		
 	}
-	return (0);				// devuelvo nulo ( en el caso que no se me cumple el while)
+	return (0);				// devuelvo null ( en el caso que no se me cumpla el while)
 }
