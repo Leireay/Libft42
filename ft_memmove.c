bc5bcdd63f5588622708541105733cb458c8a8b1
Leireay <163833943+Leireay@users.noreply.github.com>
2024-10-27 15:36:17 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -13,21 +13,27 @@
 #include "libft"
 void ft_memmove(void *dest, const char *src, size_t n)
 {
-	char	*s;
-	char	*d;
-	size_t	i;
+        size_t  i;
 
-	s = (char *)src;
-	d = (char *)dst;
-	i = 0;
-	if (d > s)
-		while (len-- > 0)
-			d[len] = s[len];
-	else
-		while (i < len)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	return (dst);
-}
+        if (!dest && !src)
+                return (0);
+        i = 0;
+        if ((size_t)dest - (size_t)src < n)
+        {
+                i = n - 1;
+                while (i < n)
+                {
+                        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+                        i++;
+                }
+        }
+        else
+        {
+                while (i < n)
+                {
+                        ((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+                        i++;
+                }
+        }
+        return (dest);
+
