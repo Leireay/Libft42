0e58d956941898fd729224152d1c8034bbc2660d
Leireay <163833943+Leireay@users.noreply.github.com>
2024-10-27 15:16:47 +0100

    Update ft_striteri.c

--- ft_striteri.c
+++ ft_striteri.c
@@ -14,14 +14,12 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	int	i;
-
-	if (!s)
-		return ;
+	size_t	i;
+	
 	i = 0;
 	while (s[i] != '\0')
 	{
-		f(i, &s[i]);
+		if(i, &s[i]);
 		i++;
 	}
 }
